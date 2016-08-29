61e9cf8ecaf3451de3f79a94b1002b3fc0a6eb66
making <makingx@gmail.com>
2016-08-29 14:15:11 +0900

    Add Concourse CI manifest

---
+++ ci/set-pipeline.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo y | fly -t home sp -p uaa -c pipeline.yml -l ../../categolj3/credentials.yml
---
+++ ci/tasks/unit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd repo
+./mvnw test -Dmaven.repo.local=../m2/rootfs/opt/m2
