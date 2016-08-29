db1591d3946a561ead70619f8c4ed1239b074f7d
making <makingx@gmail.com>
2016-08-29 17:23:05 +0900

    use wildcard

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,4 +4,4 @@ cd repo
 ./mvnw package -DskipTests=true -Dmaven.repo.local=../m2/rootfs/opt/m2
 ./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2
 artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=../m2/rootfs/opt/m2 | egrep -v '(^\[INFO])'`
-mv target/uaa.jar ../output/app.jar
+mv target/*.jar ../output/app.jar
