bc05802ab78e70deda9b4cb13796ef4b1b8990be
making <makingx@gmail.com>
2016-12-25 11:20:54 +0900

    update scripts

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 BASEDIR=`pwd`
+M2REPO=$BASEDIR/../m2/rootfs/opt/m2
+
 cd $1
-./mvnw package -DskipTests=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2
-./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2
-artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2 | egrep -v '(^\[INFO])'`
+./mvnw package -DskipTests=true -Dmaven.repo.local=$M2REPO
+./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$M2REPO
+artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$M2REPO | egrep -v '(^\[INFO])'`
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+BASEDIR=`pwd`
+M2REPO=$BASEDIR/../m2/rootfs/opt/m2
 
 cd $1
-./mvnw test -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2
+./mvnw test -Dmaven.repo.local=$M2REPO
