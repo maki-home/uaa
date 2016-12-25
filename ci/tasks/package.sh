f7d01195454b3dc986bb841fd7a62359e08e0f00
making <makingx@gmail.com>
2016-12-25 11:17:40 +0900

    Polish

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -5,4 +5,3 @@ cd $1
 ./mvnw package -DskipTests=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2
 ./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2
 artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$BASEDIR/m2/rootfs/opt/m2 | egrep -v '(^\[INFO])'`
-mv target/*.jar ${workdir}/output/app.jar
