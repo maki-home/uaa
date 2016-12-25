96319dca8148cd40d5236e5f73f2d4594d6d42ab
making <makingx@gmail.com>
2016-12-25 19:33:11 +0900

    Update scripts / pipeline

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -8,3 +8,4 @@ cd $1
 ./mvnw package -DskipTests=true -Dmaven.repo.local=$M2REPO
 ./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$M2REPO
 artifactId=`./mvnw help:evaluate -Dexpression=project.artifactId -Dmaven.test.skip=true -Dmaven.repo.local=$M2REPO | egrep -v '(^\[INFO])'`
+cd $BASEDIR
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -5,3 +5,4 @@ echo "M2REPO=$M2REPO"
 
 cd $1
 ./mvnw test -Dmaven.repo.local=$M2REPO
+cd $BASEDIR
