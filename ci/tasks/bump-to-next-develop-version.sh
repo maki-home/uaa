f62714c61fa1c51a05b39656d2fb60ab0d209e3a
making <makingx@gmail.com>
2016-12-26 20:48:13 +0900

    Add bump-to-next-dev-version

---
+++ ci/tasks/bump-to-next-develop-version.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+BASEDIR=`pwd`
+M2REPO=$BASEDIR/m2/rootfs/opt/m2
+echo "M2REPO=$M2REPO"
+VERSION=`cat version/number`-SNAPSHOT
+MESSAGE="[Concourse CI] Bump to Next Development Version ($VERSION)"
+
+shopt -s dotglob
+mv -f uaa-repo/* develop-out/
+cd develop-out
+git remote add -f prod $BASEDIR/uaa-prod
+git merge --no-edit prod/master
+echo "Bump to $VERSION"
+./mvnw versions:set -DnewVersion=${VERSION} -DallowSnapshots -Dmaven.repo.local=$M2REPO
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git add -A
+git commit -m "${MESSAGE}"
--- ci/tasks/merge-release-to-master.sh
+++ ci/tasks/merge-release-to-master.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+BASEDIR=`pwd`
 shopt -s dotglob
 mv -f uaa-repo/* master-out/
 cd master-out
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-git remote add -f staging ../uaa-repo-staging
+git remote add -f staging $BASEDIR/uaa-repo-staging
 git merge --no-edit staging/release
