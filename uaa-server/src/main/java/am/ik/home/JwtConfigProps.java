63b77e13ec0908a2226eacf84d021a3e703ec479
making <makingx@gmail.com>
2017-02-12 23:36:48 +0900

    Use @ Component instead of @ Configuration

--- uaa-server/src/main/java/am/ik/home/JwtConfigProps.java
+++ uaa-server/src/main/java/am/ik/home/JwtConfigProps.java
@@ -1,7 +1,7 @@
 package am.ik.home;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
@@ -9,7 +9,7 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "jwt")
 @Validated
 @Data
-@Configuration
+@Component
 public class JwtConfigProps {
 	private String issuer;
 	private String verifierKey;
