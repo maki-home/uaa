eaa6563e8f6fce3355110ea266d63cae8e654fd0
making <makingx@gmail.com>
2017-02-12 23:36:21 +0900

    Add @ Configuration on JwtConfigProps

--- uaa-server/src/main/java/am/ik/home/JwtConfigProps.java
+++ uaa-server/src/main/java/am/ik/home/JwtConfigProps.java
@@ -1,6 +1,7 @@
 package am.ik.home;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "jwt")
 @Validated
 @Data
+@Configuration
 public class JwtConfigProps {
 	private String issuer;
 	private String verifierKey;
